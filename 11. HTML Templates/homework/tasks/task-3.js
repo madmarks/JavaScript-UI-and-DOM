233499610351e65f659c00a78b6c5d9ef82aec95
madmarks <bratmiro@gmail.com>
2015-08-06 14:02:10 +0300

    11. HTML Templates - Homework

--- 11. HTML Templates/homework/tasks/task-3.js
+++ 11. HTML Templates/homework/tasks/task-3.js
@@ -1,9 +1,26 @@
-function solve(){
-  return function(){
-    $.fn.listview = function(data){
-      
+/* globals $ */
+/* globals handlebars */
+
+function solve() {
+    return function () {
+        //"use strict";
+
+        $.fn.listview = function (data) {
+            var $this = $(this),
+                id = "#" + $this.attr("data-template"),
+                handlebarsTemplate = $(id).html(),
+                compiledTemplate = handlebars.compile(handlebarsTemplate),
+                $documentFragment = $(document.createDocumentFragment());
+
+            data.forEach(function (item) {
+                $documentFragment.append(compiledTemplate(item));
+            });
+
+            $documentFragment.appendTo($this);
+
+            return $this;
+        };
     };
-  };
 }
 
 module.exports = solve;
